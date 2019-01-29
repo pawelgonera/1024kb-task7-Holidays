6241f610552f0c58e017f439f2e235c63afa1167
poul124 <poulg12@gmail.com>
2019-01-29 11:40:26 +0100

    added load class for matrix file

---
+++ src/main/java/pl/_1024kb/task07/exception/EmptyArrayException.java
@@ -0,0 +1,11 @@
+package pl._1024kb.task07.exception;
+
+public class EmptyArrayException extends Exception
+{
+    public EmptyArrayException() {
+    }
+
+    public EmptyArrayException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/pl/_1024kb/task07/manager/converter/GraphMatrixConverter.java
@@ -0,0 +1,6 @@
+package pl._1024kb.task07.manager.converter;
+
+public class GraphMatrixConverter
+{
+
+}
